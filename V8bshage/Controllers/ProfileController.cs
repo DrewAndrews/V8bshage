644ccf021e38e112d911f8abd6875039729b1233
DrewAndrew <andreyrusinovich1970@gmail.com>
2021-05-12 23:06:31 +0900

    Login, Register, Profile page almost done

---
+++ V8bshage/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using V8bshage.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace V8bshage.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public ProfileController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userManager.GetUserAsync(User));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ V8bshage/Models/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace V8bshage.Models
+{
+    public class News
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public byte[] Photo { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime PostTime { get; set; }
+    }
+}
