efb97f272b4892dd84122b2cf0ea2d02fe151f72
DrewAndrew <andreyrusinovich1970@gmail.com>
2021-05-16 00:45:49 +0900

    Add shop page with adding adverts

---
+++ V8bshage/Models/Advertisement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace V8bshage.Models
+{
+    public class Advertisement
+    {
+        [Key]
+        public int AdvId { get; set; }
+        [DisplayName("Название")]
+        public string Title { get; set; }
+        [DisplayName("Описание")]
+        public string Description { get; set; }
+        [DisplayName("Цена")]
+        public int Price { get; set; }
+        [DisplayName("Фото")]
+        public byte[] Photo { get; set; }
+        public string UserId { get; set; }
+    }
+}
